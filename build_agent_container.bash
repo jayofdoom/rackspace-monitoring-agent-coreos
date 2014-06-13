5e1ce18f652383ad7b4e668bd79be40ccfb93392
Jay Faulkner <jay@jvf.cc>
2014-06-13 18:49:43 +0000

    more

--- build_agent_container.bash
+++ build_agent_container.bash
@@ -2,7 +2,7 @@
 
 CONTAINER_UUID=`uuidgen`
 
-docker build -t rackspace-monitoring-agent .
+docker build -rm -t rackspace-monitoring-agent .
 docker run rackspace-monitoring-agent echo ${CONTAINER_UUID}
 CONTAINER=`docker ps -a --no-trunc |grep ${CONTAINER_UUID}|awk '{print $1}'|head -n1`
 docker export ${CONTAINER} | xz > rackspace-monitoring-agent-container.tar.xz
