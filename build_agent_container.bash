7c24bb1e842bce2568cf1debaeb5cf4d2f90a302
Jay Faulkner <jay@jvf.cc>
2014-06-13 18:17:10 +0000

    Initial Commit

---
+++ build_agent_container.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+CONTAINER_UUID=`uuidgen`
+
+docker build -t rackspace-monitoring-agent .
+docker run rackspace-monitoring-agent echo ${CONTAINER_UUID}
+CONTAINER=`docker ps -a --no-trunc |grep ${CONTAINER_UUID}|awk '{print $1}'|head -n1`
+docker export ${CONTAINER} | xz > rackspace-monitoring-agent-container.tar.xz
